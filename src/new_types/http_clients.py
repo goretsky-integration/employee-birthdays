f50b002f5f2d89e81d7ac06cd9cd28864bc31143
Eldos <eldos.baktybekov@gmail.com>
2024-03-01 23:06:49 +0600

    :label: Add auth credentials storage connection http client type

--- src/new_types/http_clients.py
+++ src/new_types/http_clients.py
@@ -2,6 +2,12 @@ from typing import NewType
 
 import httpx
 
-__all__ = ('DodoISConnectionHttpClient',)
+__all__ = (
+    'AuthCredentialsStorageConnectionHttpClient',
+    'DodoISConnectionHttpClient',
+)
 
+AuthCredentialsStorageConnectionHttpClient = (
+    NewType('AuthCredentialsStorageConnectionHttpClient', httpx.Client)
+)
 DodoISConnectionHttpClient = NewType('DodoISConnectionHttpClient', httpx.Client)
