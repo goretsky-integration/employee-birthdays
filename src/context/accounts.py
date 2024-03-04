cc0d5d0cf94d4799206b90f53a958179264a0c28
Eldos <eldos.baktybekov@gmail.com>
2024-03-04 10:00:34 +0600

    :construction: Add accounts context

---
+++ src/context/accounts.py
@@ -0,0 +1,35 @@
+from collections.abc import Generator, Iterable
+
+from auth_credentials_storage import AuthCredentialsStorageConnection
+from models import Account, AccountCookies
+from new_types import AuthCredentialsStorageConnectionHttpClient
+from parsers import (
+    filter_office_manager_accounts,
+    parse_account_cookies_response,
+    parse_accounts_response,
+)
+
+__all__ = ('get_accounts', 'iter_accounts_cookies', 'to_account_names')
+
+
+def get_accounts(
+        http_client: AuthCredentialsStorageConnectionHttpClient,
+) -> list[Account]:
+    connection = AuthCredentialsStorageConnection(http_client)
+    accounts_response = connection.get_accounts()
+    accounts = parse_accounts_response(accounts_response)
+    return filter_office_manager_accounts(accounts)
+
+
+def iter_accounts_cookies(
+        account_names: Iterable[str],
+        http_client: AuthCredentialsStorageConnectionHttpClient,
+) -> Generator[AccountCookies, None, None]:
+    connection = AuthCredentialsStorageConnection(http_client)
+    for account_name in account_names:
+        account_cookies_response = connection.get_cookies(account_name)
+        yield parse_account_cookies_response(account_cookies_response)
+
+
+def to_account_names(accounts: Iterable[Account]) -> set[str]:
+    return {account.name for account in accounts}
